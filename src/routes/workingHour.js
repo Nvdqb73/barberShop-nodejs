f17b0726c45d49a2e3df71c521f40cca814da105
Nvdqb73 <nvdqb73@gmail.com>
2024-04-10 00:36:29 +0700

    fix route working hour

--- src/routes/workingHour.js
+++ src/routes/workingHour.js
@@ -7,8 +7,8 @@ const { validateTimeWorkingHour } = require('../app/middlewares/validateTime');
 const workingHourController = require('../app/controllers/WorkingHourController');
 
 router.post('/create', [verifyAccessToken, isAdmin, validateTimeWorkingHour], workingHourController.create);
-router.put('/update/:whId', [verifyAccessToken, isAdmin, validateTimeWorkingHour], workingHourController.update);
-router.delete('/delete/:whId', [verifyAccessToken, isAdmin], workingHourController.delete);
 router.get('/', workingHourController.getAll);
 router.get('/:whId', workingHourController.getById);
+router.put('/:whId', [verifyAccessToken, isAdmin, validateTimeWorkingHour], workingHourController.update);
+router.delete('/:whId', [verifyAccessToken, isAdmin], workingHourController.delete);
 module.exports = router;
